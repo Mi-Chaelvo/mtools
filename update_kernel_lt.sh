a7a2a187c1aa9e821d484b1e9493344e1c5e3898
Mi-Chaelvo <38527468+Mi-Chaelvo@users.noreply.github.com>
2023-06-29 10:27:04 +0800

    Update update_kernel_lt.sh

--- update_kernel_lt.sh
+++ update_kernel_lt.sh
@@ -5,6 +5,6 @@ rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
 echo "Start installing kernel boot!"
 yum install https://www.elrepo.org/elrepo-release-7.el7.elrepo.noarch.rpm -y
 echo "Starting to install the kernel!"
-yum --enablerepo=elrepo-kernel install kernel-lt kernel-lt-devel kernel-lt-headers kernel-lt-tools kernel-lt-tools-libs kernel-lt-tools-libs-devel -y
+yum --enablerepo=elrepo-kernel install kernel-lt kernel-lt-devel kernel-lt-headers -y
 echo "Set boot to start!"
 grub2-set-default 0
